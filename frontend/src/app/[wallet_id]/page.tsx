ccd00a1b5d07a1c37a6ce069345ddf089af45fbd
Lara Carvalho <laraftoller@gmail.com>
2023-06-29 20:48:40 -0300

    feat: add main page and homebroker page

---
+++ frontend/src/app/[wallet_id]/page.tsx
@@ -0,0 +1,14 @@
+import MyWallet from '../components/MyWallet'
+
+export default async function HomePage({
+	params,
+}: {
+	params: { wallet_id: string };
+}) {
+	return (
+		<div>
+			<h1>Meus investimentos</h1>
+      <MyWallet wallet_id={params.wallet_id}></MyWallet>
+		</div>
+	);
+}
